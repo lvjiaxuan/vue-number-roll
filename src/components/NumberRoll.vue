5ac29445d026ee7c6785f152e9ac234af02d9e89
lvjiaxuan <471501748@qq.com>
2021-04-05 18:35:47 +0800

    feat: （rebased）ts改造中

---
+++ src/components/NumberRoll.vue
@@ -0,0 +1,31 @@
+<template>
+  <p>xxx</p>
+</template>
+
+<script setup="props" lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.base-num-roll {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  overflow: hidden;
+  display: inline-flex;
+  li {
+    transition-property: transform;
+    box-sizing: border-box;
+    margin-top: 0 !important;
+    margin-bottom: 0 !important;
+    p {
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      box-sizing: border-box;
+    }
+  }
+}
+</style>
