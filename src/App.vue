3930ab6ae2640492957f48a0305437146def0dd3
lvjx7 <471501748@qq.com>
2021-06-24 17:42:27 +0800

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <number-roll />
+
+  <header>vue-number-roll</header>
+
+  <number-roll start-number="11" roll-height="22px" item-class="num-item" />
 </template>
 
 <script setup lang="ts">
@@ -8,83 +11,6 @@ import NumberRoll from './components/NumberRoll.vue'
 </script>
 
 <style lang="scss">
-.title {
-  text-align: center;
-  color: #5026a7;
-  font-size: 60px;
-}
-.container {
-  text-align: center;
-}
-.options {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  margin: 20px 0;
-  label {
-    display: inline-block;
-    margin: 15px 30px 15px 0;
-  }
-  input[type=text], input[type=number] {
-    display: inline-block;
-    padding: 4px 7px;
-    width: 70px;
-    height: 25px;
-    cursor: text;
-    font-size: 12px;
-    line-height: 1.5;
-    color: rgba(0, 0, 0, .65);
-    background-color: #fff;
-    background-image: none;
-    border: 1px solid #d9d9d9;
-    border-radius: 4px;
-    transition: all .3s;
-  }
-  input[type=checkbox] {
-    margin: 0;
-    width: 25px;
-    height: 25px;
-  }
-}
-button {
-  margin-top: 10px;
-  color: #470938;
-  width: 100%;
-  margin-bottom: 0;
-  font-weight: 500;
-  text-align: center;
-  touch-action: manipulation;
-  cursor: pointer;
-  background-image: none;
-  border: 1px solid transparent;
-  white-space: nowrap;
-  line-height: 1.5;
-  padding: 10px 15px;
-  font-size: 16px;
-  border-radius: 4px;
-  user-select: none;
-  transition: all .3s cubic-bezier(.645, .045, .355, 1);
-  background-color: #fff;
-  border-color: #d9d9d9;
-  &:hover {
-    color: #1a3e59;
-    background-color: #f2d6eb;
-  }
-}
-code {
-  margin-top: 25px;
-  display: block;
-  padding: 8px 15px;
-  background-color: #f6f8fa;
-  border-radius: 5px;
-  border: 1px solid #e5e5e5;
-  overflow-x: auto;
-  color: #333;
-  font-size: 14px;
-  &.css-style {
-    white-space: pre-wrap;
-  }
-}
 .num-item {
   border-radius: 8px;
   width: 100px;
--- src/components/NumberRoll.vue
+++ src/components/NumberRoll.vue
@@ -2,11 +2,92 @@
   <p>xxx</p>
 </template>
 
-<script setup="props" lang="ts">
+<script setup lang="ts">
+import { computed, defineProps, onBeforeMount, onMounted, watch, watchEffect } from 'vue'
 
+const props = defineProps({
+  startNumber: {
+    type: [Number, String],
+    default: 0,
+    validator(value: number | string) {
+      return Number.isInteger(+value) && +value >= 0
+    },
+  },
+  endNumber: {
+    type: [Number, String],
+    default: 3000,
+    validator(value: number | string) {
+      return Number.isInteger(+value) && +value >= 0
+    },
+  },
+  timeFunction: {
+    type: String,
+    default: 'ease-in-out',
+    validator(value: string) {
+      return (
+        ['linear', 'ease', 'ease-in', 'ease-out', 'ease-in-out'].includes(value) ||
+        value.includes('cubic-bezier')
+      )
+    },
+  },
+  minLength: {
+    type: [Number, String],
+    default: 0,
+    validator(value: number | string) {
+      return Number.isInteger(+value) && +value >= 0
+    },
+  },
+  rollHeight: {
+    type: String,
+    required: true,
+    validator(value: string) {
+      return /^\d+(px|rem|em|%)+$/g.test(value)
+    },
+  },
+  reverse: {
+    type: Boolean,
+    default: false,
+  },
+  itemClass: {
+    type: String,
+    required: true,
+  },
+  autoplay: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+let liTranslate: string[] = []
+
+const rollNumber = computed(() => props.endNumber.toString().padStart(+props.minLength, '0'))
+const rollHeightUnit = computed(() => props.rollHeight.replace(/\d/g, ''))
+const rollHeightNumber = computed(() => props.rollHeight.replace(new RegExp(rollHeightUnit.value, 'g'), ''))
+
+watch([props.startNumber, props.rollHeight, props.minLength], init, { immediate: true })
+
+onBeforeMount(init)
+onMounted(() => props.autoplay && setTimeout(start))
+
+/**
+ * methods
+ */
+
+function init() {
+  liTranslate = []
+  props.startNumber.toString().padStart(+props.minLength, '0').split('').forEach((item, idx) => setLiTranslate(idx, item))
+}
+
+function start() {
+
+}
+
+function setLiTranslate(idx: number, item: number | string) {
+  // this.$set(this.liTranslate, idx, { transform: 'translateY(' + ((this.reverse ? ((9 - +item) * -this.rollHeightNum) : (-item * this.rollHeightNum))) + this.rollHeighUnit + ')' });
+}
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .base-num-roll {
   margin: 0;
   padding: 0;
