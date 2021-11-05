99d48762a87514c2574e500b54a663f0d7cbbc28
lvjx7 <lvjx7@meicloud.com>
2021-11-05 14:18:16 +0800

    build: 分开lib构建

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,9 +1,17 @@
+import path from 'path'
 import ScriptSetup from 'unplugin-vue2-script-setup/vite'
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/components/NumberRoll.vue'),
+      name: 'NumberRoll',
+      fileName: format => `index.${format}.js`,
+    },
+  },
   plugins: [
     createVuePlugin(),
     ScriptSetup({
