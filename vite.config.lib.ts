9ccda3d8ac24785d23d9618ce94b7ae64e549705
lvjx7 <lvjx7@meicloud.com>
2022-03-07 17:17:06 +0800

    build: 升级依赖版本

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// https://cn.vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
   build: {
     lib: {
