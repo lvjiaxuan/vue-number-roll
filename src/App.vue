e35f60f0ba45e7f2dc00de1d8e4005886b9f5b67
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2023-04-11 15:50:52 +0800

    feat: works!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,221 @@
 <script setup lang="ts">
+import NumberRoll from './NumberRoll.vue'
+import { computed, reactive, ref, watch } from 'vue'
+
+/**
+ * data
+ */
+const numberRollRef = ref<InstanceType<typeof NumberRoll>>()
+const options = reactive({
+  startNumber: {
+    value: 123,
+    type: 'number',
+  },
+  endNumber: {
+    value: 9876,
+    type: 'number',
+  },
+  transitionDelay: {
+    value: 0,
+    type: 'number',
+  },
+  transitionDuration: {
+    value: 2555,
+    type: 'number',
+  },
+  transitionTimingFunction: {
+    value: 'ease-in-out',
+    type: 'text',
+  },
+  minLength: {
+    value: 5,
+    type: 'number',
+  },
+  rollHeight: {
+    value: '120px',
+    type: 'text',
+  },
+  reverseDirection: {
+    value: false,
+    type: 'checkbox',
+  },
+  autoplay: {
+    value: false,
+    type: 'checkbox',
+  },
+})
+
+const htmlCode = computed(
+  () => `<number-roll
+    startNumber="${ options.startNumber.value }"
+    endNumber="${ options.endNumber.value }"
+    min-length="${ options.minLength.value }"
+    roll-height="${ options.rollHeight.value }"
+    transition-delay="${ options.transitionDelay.value }"
+    transition-duration="${ options.transitionDuration.value }"
+    transition-time-function="${ options.transitionTimingFunction.value }"
+    number-class="num-item"
+    ${ options.autoplay.value ? 'autoplay' : '' }
+    ${ options.reverseDirection.value ? 'reverse-irection' : '' } />`,
+)
+
+watch(
+  () => options.autoplay.value,
+  newValue => sessionStorage.setItem('autoplay', newValue.toString()),
+)
+
+options.autoplay.value = sessionStorage.getItem('autoplay') === 'true'
+
+const go = () => {
+  numberRollRef.value.start()
+}
+
+const reset = () => {
+  // @ts-ignore
+  numberRollRef.value.reset()
+}
 </script>
 
 <template>
-  Halo
+  <main>
+    <h1 class="title">
+      vue-number-roll
+    </h1>
+
+    <section class="container">
+      <number-roll
+        ref="numberRollRef"
+        :start-number="options.startNumber.value"
+        :end-number="options.endNumber.value"
+        :min-length="options.minLength.value"
+        :roll-height="options.rollHeight.value"
+        :transition-delay="options.transitionDelay.value"
+        :transition-duration="options.transitionDuration.value"
+        :transition-time-function="options.transitionTimingFunction.value"
+        :autoplay="options.autoplay.value"
+        :reverse-direction="options.reverseDirection.value"
+        number-class="num-item"
+      />
+    </section>
+
+    <section class="options">
+      <label
+        v-for="(value, key) in options"
+        :key="key"
+        :for="key"
+      >
+        {{ key }}:<input
+          :id="key"
+          v-model="value.value"
+          :type="value.type"
+        >
+      </label>
+    </section>
+
+    <button @click="go">
+      Let's roll
+    </button>
+    <button @click="reset">
+      Reset
+    </button>
+
+    <code>{{ htmlCode }}</code>
+
+    <code class="css-style">
+      .num-item { border-radius: 8px; width: 100px; margin: 10px; background: linear-gradient(0deg, rgba(100, 184, 255,
+      1) 0%, rgba(0, 120, 255, 1) 50%, rgba(100, 184, 255, 1) 100%); font-size: 80px; color: #fff; }</code>
+  </main>
 </template>
+
+<style lang="scss">
+main {
+  width: 720px;
+  margin: 0 auto;
+}
+.title {
+  text-align: center;
+  color: #5026a7;
+  font-size: 60px;
+}
+.container {
+  text-align: center;
+}
+.options {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 20px 0;
+  label {
+    display: inline-block;
+    margin: 15px 30px 15px 0;
+  }
+  input[type='text'],
+  input[type='number'] {
+    display: inline-block;
+    padding: 4px 7px;
+    width: 70px;
+    height: 25px;
+    cursor: text;
+    font-size: 12px;
+    line-height: 1.5;
+    color: rgba(0, 0, 0, 0.65);
+    background-color: #fff;
+    background-image: none;
+    border: 1px solid #d9d9d9;
+    border-radius: 4px;
+    transition: all 0.3s;
+  }
+  input[type='checkbox'] {
+    margin: 0;
+    width: 25px;
+    height: 25px;
+  }
+}
+button {
+  margin-top: 10px;
+  color: #470938;
+  width: 100%;
+  margin-bottom: 0;
+  font-weight: 500;
+  text-align: center;
+  touch-action: manipulation;
+  cursor: pointer;
+  background-image: none;
+  border: 1px solid transparent;
+  white-space: nowrap;
+  line-height: 1.5;
+  padding: 10px 15px;
+  font-size: 16px;
+  border-radius: 4px;
+  user-select: none;
+  transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+  background-color: #fff;
+  border-color: #d9d9d9;
+  &:hover {
+    color: #1a3e59;
+    background-color: #f2d6eb;
+  }
+}
+code {
+  margin-top: 25px;
+  display: block;
+  padding: 8px 15px;
+  background-color: #f6f8fa;
+  border-radius: 5px;
+  border: 1px solid #e5e5e5;
+  overflow-x: auto;
+  color: #333;
+  font-size: 14px;
+  &.css-style {
+    white-space: pre-wrap;
+  }
+}
+.num-item {
+  border-radius: 8px;
+  width: 100px;
+  margin: 10px;
+  background: linear-gradient(0deg, rgba(100, 184, 255, 1) 0%, rgba(0, 120, 255, 1) 50%, rgba(100, 184, 255, 1) 100%);
+  font-size: 80px;
+  color: #fff;
+}
+</style>
--- src/NumberRoll.vue
+++ src/NumberRoll.vue
@@ -1,7 +1,150 @@
 <script lang="ts" setup>
+import { computed, onMounted, ref, toRefs, watch } from 'vue'
 
+const props = defineProps({
+  startNumber: {
+    type: [ Number, String ],
+    default: 0,
+    validator: (value: number | string) => Number.isInteger(+value) && +value >= 0,
+  },
+  endNumber: {
+    type: [ Number, String ],
+    default: 0,
+    validator: (value: number | string) => Number.isInteger(+value) && +value >= 0,
+  },
+  minLength: {
+    type: [ Number, String ],
+    default: 0,
+    validator: (value: number | string) => Number.isInteger(+value) && +value >= 0,
+  },
+  rollHeight: {
+    type: String,
+    required: true,
+  },
+  reverseDirection: {
+    type: Boolean,
+    default: false,
+  },
+  autoplay: {
+    type: Boolean,
+    default: false,
+  },
+  numberClass: {
+    type: String,
+    default: '',
+  },
+  transitionDuration: {
+    type: [ Number, String ],
+    default: 3000,
+    validator: (value: number | string) => Number.isInteger(+value) && +value >= 0,
+  },
+  transitionTimingFunction: {
+    type: String,
+    default: 'ease-in-out',
+    validator: (value: string) =>
+      [ 'linear', 'ease', 'ease-in', 'ease-out', 'ease-in-out' ].includes(value) || value.includes('cubic-bezier'),
+  },
+  transitionDelay: {
+    type: [ Number, String ],
+    default: 0,
+    validator: (value: number | string) => Number.isInteger(+value) && +value >= 0,
+  },
+})
+
+
+const splitNumbers = ref<{ transform: string }[]>([])
+function init() {
+  splitNumbers.value.length = 0
+  props.startNumber
+    .toString()
+    .padStart(+props.minLength, '0')
+    .split('')
+    .forEach((number, idx) => setSingleTranslate(idx, +number))
+}
+
+
+const endNumberPadding = computed(() => props.endNumber.toString().padStart(+props.minLength, '0'))
+function start() {
+  splitNumbers.value.length = 0
+  endNumberPadding.value.split('').forEach((number, idx) => setSingleTranslate(idx, +number))
+}
+
+const rollHeightUnit = computed(() => props.rollHeight.replace(/\d/g, ''))
+const rollHeightNumber = computed(() => +props.rollHeight.replace(new RegExp(rollHeightUnit.value, 'g'), ''))
+function setSingleTranslate(idx: number, number: number) {
+  splitNumbers.value[idx] = {
+    transform: `translateY(${
+      (props.reverseDirection ? (+number - 9) * rollHeightNumber.value : -number * rollHeightNumber.value) +
+      rollHeightUnit.value
+    })`,
+  }
+}
+
+/**
+ * watch
+ */
+watch([ () => props.startNumber, () => props.rollHeight, () => props.minLength, () => props.reverseDirection ], init, { immediate: true })
+
+/**
+ * life cycle
+ */
+onMounted(() => props.autoplay && window.requestAnimationFrame(start))
+
+/**
+ * expose
+ */
+defineExpose({ start, reset: init })
 </script>
 
 <template>
-  <main>halo, world</main>
+  <ul
+    class="base-num-roll"
+    :style="{ height: rollHeight }"
+  >
+    <li
+      v-for="index in splitNumbers.length"
+      :key="index"
+      :class="numberClass"
+    >
+      <div
+        :style="[
+          splitNumbers[index - 1],
+          { 'transition-duration': +transitionDuration / 1000 + 's' },
+          { 'transition-timing-function': transitionTimingFunction },
+        ]"
+      >
+        <p
+          v-for="n in 10"
+          :key="n"
+          :style="{ height: rollHeight }"
+        >
+          {{ reverseDirection ? 10 - n : n - 1 }}
+        </p>
+      </div>
+    </li>
+  </ul>
 </template>
+
+<style lang="scss">
+.base-num-roll {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  overflow: hidden;
+  display: inline-flex;
+  li {
+    transition-property: transform;
+    box-sizing: border-box;
+    margin-top: 0 !important;
+    margin-bottom: 0 !important;
+    p {
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      box-sizing: border-box;
+    }
+  }
+}
+</style>
