a1b90d45170e5b5b61924b85ae8c0a558c9d91a4
unknown <11309921+lvjiaxuan@users.noreply.github.com>
2023-05-10 00:14:35 +0800

    chore: vue3 playground

---
+++ playgrounds/vue3/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'virtual:uno.css'
+import '@unocss/reset/normalize.css'
+
+createApp(App).mount('#app')
--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -15,7 +15,7 @@ export default mergeConfig(base, {
       name: 'NumberRoll',
     },
     rollupOptions: {
-      external: [ 'vue' ],
+      external: [ 'vue', 'vue-demi' ],
       output: {
       // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: { vue: 'Vue' },
