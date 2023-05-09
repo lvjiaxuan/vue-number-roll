74d208f0eff91ecbe0459df60614ddb500c82989
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2023-05-09 18:20:19 +0800

    chore: i cssInjectedByJsPlugin

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -3,6 +3,7 @@ import base from './vite.config'
 import path from 'node:path'
 import dts from 'vite-plugin-dts'
 import pkg from './package.json' assert { type: 'json' }
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default mergeConfig(base, {
   base: '/vue-number-roll',
@@ -13,7 +14,13 @@ export default mergeConfig(base, {
       entry: path.resolve(__dirname, './src/NumberRoll.ts'),
       name: 'NumberRoll',
     },
-    rollupOptions: { external: [ 'vue' ] },
+    rollupOptions: {
+      external: [ 'vue' ],
+      output: {
+      // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: { vue: 'Vue' },
+      },
+    },
   },
   plugins: [
     dts({
@@ -21,5 +28,6 @@ export default mergeConfig(base, {
         return { filePath: path.join(__dirname, pkg.types), content }
       },
     }),
+    cssInjectedByJsPlugin(),
   ],
 } as UserConfig)
